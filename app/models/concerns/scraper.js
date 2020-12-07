69729404ccdb649b7f4b9ad471bc33a76cca41da
Marisa You <marisayou2017@u.northwestern.edu>
2020-12-06 22:47:19 -0800

    remove scraper from gitignore, fix bug to allow prices over 1000

---
+++ app/models/concerns/scraper.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const file = 'app/models/concerns/search_results.txt';
+
+const { firefox } = require('playwright');
+const queryTerm = process.argv.slice(2);
+
+(async () => {
+  fs.writeFile(file, '', err => { if (err) throw err });
+  const browser = await firefox.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(`https://www.nordstrom.com/sr?origin=keywordsearch&keyword=${queryTerm}`);
+  await page.innerHTML('main#layer-0');
+  
+  let hasResults = await page.$('article.QIjwE');
+  if (hasResults) {
+    results = await page.$$('article.QIjwE');
+    const displayNum = results.length < 12 ? results.length : 12;
+    for (let i = 0; i < displayNum; i++) {
+      
+      const nameLink = await results[i].$('a._5lXiG');
+      const name = await nameLink.innerText();
+      const link = await nameLink.getAttribute('href');
+      
+      let priceDiv = await results[i].$('div._18N5Q') || await results[i].$('div._3bi0z');
+      
+      const priceSpan = await priceDiv.$('span._3wu-9');
+      const priceStr = await priceSpan.innerText();
+      const price = parseFloat(priceStr.slice(1).replace(',', ''));
+
+      const img = await results[i].$('img.TDd9E');
+      const imgURL = await img.getAttribute('src');
+
+      fs.appendFile(file, `${name}<|>${price}<|>https://nordstrom.com${link}<|>${imgURL}\n`, err => { if (err) throw err });
+    }
+  }
+
+  await browser.close();
+})();
+
